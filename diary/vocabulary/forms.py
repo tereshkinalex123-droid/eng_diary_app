5a0b51db78a370a648f747a34089462056bb4a64
Alex <tereshkinalex123@gmail.com>
2026-03-02 17:23:17 +0300

    feat(vocabulary): add forms for creating decks and cards.

---
+++ diary/vocabulary/forms.py
@@ -0,0 +1,39 @@
+from html.parser import attrfind_tolerant
+
+from django import forms
+from .models import Card, Deck
+
+class CardForm(forms.ModelForm):
+    class Meta:
+        model = Card
+        fields = ('front', 'back', 'examples', 'deck')
+        widgets = {
+            'front': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введи переднюю сторону карточки',
+            }),
+            'back': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введи заднюю сторону карточки',
+            }),
+            'examples': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введи примеры',
+            }),
+            'deck': forms.Select(attrs={'class': 'form-control'})
+        }
+
+        def __init__(self, *args, user=None, **kwargs):
+            super().__init__(*args, **kwargs)
+            if user:
+                self.fields['deck'].queryset = Deck.objects.filter(user=user)
+
+class DeckForm(forms.ModelForm):
+    class Meta:
+        model = Deck
+        fields = ('name',)
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Введи название колоды',
+            })}
