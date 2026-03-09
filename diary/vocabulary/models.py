00c5293489cc6175fc15b6f28711689e64892c49
Alex <tereshkinalex123@gmail.com>
2026-03-09 19:24:59 +0300

    feat(vocabulary): add ReviewSession and ReviewSessionCard models

--- diary/vocabulary/models.py
+++ diary/vocabulary/models.py
@@ -10,7 +10,7 @@ User = get_user_model()
 class Deck(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='decks')
     name = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -19,41 +19,20 @@ class Deck(models.Model):
                 Lower('name'),
                 'user',
                 name='unique_deck_per_user',
-            )
-        ]
-
-    def __str__(self):
-        return f"({self.user.name}){self.name}"
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(f"({self.user.name}){self.name}")
-        super().save(*args, **kwargs)
-
-class Card(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='decks')
-    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name='decks')
-    slug = models.SlugField(max_length=50, unique=True)
-    front = models.CharField(max_length=50)
-    back = models.CharField(max_length=50)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"({self.user.name}){self.front}"
-
-    class Meta:
-        constraints = [
+            ),
             models.UniqueConstraint(
-                'slug',
                 'user',
-                name='unique_deck_per_user',
+                'slug',
+                name='unique_deck_slug_per_user'
             )
         ]
 
+    def __str__(self):
+        return f"{self.user.username}: {self.name}"
+
     def save(self, *args, **kwargs):
         if not self.slug:
-            base_slug = slugify(f"({self.user.name}){self.front}")
+            base_slug = slugify(f"{self.front}")
             slug = base_slug
             counter = 1
 
@@ -62,15 +41,115 @@ class Card(models.Model):
                 counter +=1
 
             self.slug = slug
+        super().save(*args, **kwargs)
+
+class Card(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cards')
+    deck = models.ForeignKey(Deck, null=True, blank=True, related_name='cards')
+    slug = models.SlugField(max_length=50, unique=True)
+    front = models.CharField(max_length=50)
+    back = models.CharField(max_length=50)
+    examples = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.name}: {self.front}"
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                'slug',
+                'user',
+                name='unique_card_slug_per_user',
+            )
+        ]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(f"{self.front}")
+            slug = base_slug
+            counter = 1
+
+            while Card.objects.filter(slug=slug,user=self.user).exists():
+                slug = f"{base_slug}-{counter}"
+                counter +=1
+
+            self.slug = slug
+        super().save(*args, **kwargs)
+
 
 class Review(models.Model):
-    card = models.OneToOneField(Card, on_delete=models.CASCADE, related_name='decks')
+    card = models.OneToOneField(
+        Card,
+        on_delete=models.CASCADE,
+        related_name='review'
+    )
 
     interval = models.IntegerField(default=1)
-    repetition = models.FloatField(default=0)
+    repetitions = models.IntegerField(default=0)
     ease_factor = models.FloatField(default=2.5)
 
     next_review = models.DateTimeField(default=timezone.now)
+    last_review = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"review for {self.card.front}"
+        return f"Review for {self.card.front}"
+
+class ReviewSession(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='review_sessions'
+    )
+
+    deck = models.ForeignKey(
+        Deck,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+    total_cards = models.IntegerField()
+    correct_answers = models.IntegerField(default=0)
+    incorrect_answers = models.IntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"ReviewSession({self.user.username})"
+
+class ReviewSessionCard(models.Model):
+
+    session = models.ForeignKey(
+        ReviewSession,
+        on_delete=models.CASCADE,
+        related_name='session_cards'
+    )
+
+    card = models.ForeignKey(
+        Card,
+        on_delete=models.CASCADE,
+    )
+
+    order = models.IntegerField()
+
+    answered = models.BooleanField(default=False)
+
+    RATING_CHOICES = [
+        ("again", "Again"),
+        ("hard", "Hard"),
+        ("good", "Good"),
+        ("easy", "Easy"),
+    ]
+
+    rating = models.CharField(
+        max_length=10,
+        choices=RATING_CHOICES,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ['order']
+        unique_together = ('session', 'card')
