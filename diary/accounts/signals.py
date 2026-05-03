401f6f9d20b53ebf41f3a1a4daf22f946298b891
Alex <tereshkinalex123@gmail.com>
2026-05-03 20:11:53 +0300

    fix(vocabulary): fix bug with new user's streak

--- diary/accounts/signals.py
+++ diary/accounts/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from vocabulary.models import CardProgress
 from records.models import Record
+from accounts.models import UserStreak
+from django.contrib.auth.models import User
 from .utils import update_streak
 
 @receiver(post_save, sender=CardProgress)
@@ -13,3 +15,8 @@ def streak_on_card_review(sender, instance, created, **kwargs):
 def streak_on_record_create(sender, instance, created, **kwargs):
     if created:
         update_streak(instance.user)
+
+@receiver(post_save, sender=User)
+def create_user_streak(sender, instance, created, **kwargs):
+    if created:
+        UserStreak.objects.create(user=instance)
