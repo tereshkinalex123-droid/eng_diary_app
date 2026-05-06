21f46f988cfd4fd5a9f5cf538e9d8c0f5e427914
Alex <tereshkinalex123@gmail.com>
2026-05-06 22:15:09 +0300

    !!!!!!!!!!!dfsdfsdaafdsf

--- diary/diary/settings.py
+++ diary/diary/settings.py
@@ -91,7 +91,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
